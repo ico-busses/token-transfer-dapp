2395077ac37241b929340d560df150638f1d0a57
adibas03 <adibas03@ymail.com>
2019-01-13 20:54:31 +0100

    [UPDATE] Latest packages (#26)
    * update all packages
    
    * missed dependency

--- src/components/HasAlert.js
+++ src/components/HasAlert.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class HasAlert extends Component {
     notify = ({ msg, type='error', autoClose= false  }) => {
