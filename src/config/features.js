334bfeea1c6d8aa6257e3984ee252da2b3e48d48
nelo <nelokwaz@gmail.com>
2019-03-15 15:40:49 +0100

    removed the extra logo images

--- src/config/features.js
+++ src/config/features.js
@@ -3,25 +3,5 @@ export default [
         text: 'StateOfTheDApps',
         link: 'https://www.stateofthedapps.com/dapps/token-transfer-dapp',
         image: 'https://www.stateofthedapps.com/_nuxt/img/2a7584b.png'
-    },
-    {
-        text: 'StateOfTheDApps',
-        link: 'https://www.stateofthedapps.com/dapps/token-transfer-dapp',
-        image: 'https://www.stateofthedapps.com/_nuxt/img/2a7584b.png'
-    },
-    {
-        text: 'StateOfTheDApps',
-        link: 'https://www.stateofthedapps.com/dapps/token-transfer-dapp',
-        image: 'https://www.stateofthedapps.com/_nuxt/img/2a7584b.png'
-    },
-    {
-        text: 'StateOfTheDApps',
-        link: 'https://www.stateofthedapps.com/dapps/token-transfer-dapp',
-        image: 'https://www.stateofthedapps.com/_nuxt/img/2a7584b.png'
-    },
-    {
-        text: 'StateOfTheDApps',
-        link: 'https://www.stateofthedapps.com/dapps/token-transfer-dapp',
-        image: 'https://www.stateofthedapps.com/_nuxt/img/2a7584b.png'
     }
 ];
